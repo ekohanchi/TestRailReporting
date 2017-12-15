f66ffb7a67e9aef2cd762510319a02750509fc8d
Kanstantsin Sudzilouski <Kanstantsin_Sudzilouski@epam.com>
2017-12-15 17:42:09 +0300

    Revert "Exclude "Not Automatable" test case type from total number"
    This reverts commit 31d510b5c660fbe3ad98e82364e9728807d23c5b.

--- src/main/java/com/project/testrail/TestCases.java
+++ src/main/java/com/project/testrail/TestCases.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.project.testrail.core.APIClient;
 
 public class TestCases {
@@ -26,14 +25,7 @@ public class TestCases {
 				Object object = client.sendGet("get_cases/" + projectId);
 				JsonElement casesJE = Utils.convertObjectToJson(object);
 				JsonArray casesJA = casesJE.getAsJsonArray();
-				tcCount = 0;
-				for (JsonElement testCase : casesJA) {
-					int testCaseTypeId = ((JsonObject)testCase).get("type_id").getAsInt();
-					// 13 is "Not Automatable" test case type
-					if (testCaseTypeId != 13) {
-						tcCount++;
-					}
-				}
+				tcCount = casesJA.size();
 			}
 
 			casesPerProject.put(projectId, tcCount);
